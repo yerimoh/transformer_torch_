932c0c0a09ec8bfa20e046a54deef6285195f6bf
yerimOh <bora3328@naver.com>
2022-08-21 05:40:44 +0900

    Update graph.py

--- graph.py
+++ graph.py
@@ -18,20 +18,19 @@ def draw(mode):
     if mode == 'loss':
         train = read('./result/train_loss.txt')
         test = read('./result/test_loss.txt')
-        plt.plot(train, 'r', label='train')
-        plt.plot(test, 'b', label='validation')
-        plt.legend(loc='lower left')
+        plt.plot(train, 'g', label='train')
+        plt.plot(test, 'r', label='validation')
+        plt.legend(loc='upper right')
 
 
     elif mode == 'bleu':
         bleu = read('./result/bleu.txt')
-        plt.plot(bleu, 'b', label='bleu score')
-        plt.legend(loc='lower right')
+        plt.plot(bleu, 'r', label='bleu score')
+        plt.legend(loc='upper right')
 
     plt.xlabel('epoch')
     plt.ylabel(mode)
     plt.title('training result')
-    plt.grid(True, which='both', axis='both')
     plt.show()
 
 
