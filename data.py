c0c01dee5bd9d12d8bcbb0131c6c125a8bceb106
yerimOh <bora3328@naver.com>
2022-08-21 05:45:31 +0900

    Update data.py

--- data.py
+++ data.py
@@ -1,8 +1,4 @@
-"""
-@author : Hyunwoong
-@when : 2019-10-29
-@homepage : https://github.com/gusdnd852
-"""
+
 from hyper import *
 from util.data_loader import DataLoader
 from util.tokenizer import Tokenizer
